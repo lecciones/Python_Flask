cee128a91fd602e7bb8c68754d26fb4c8856c3e6
Laravel <johnmikoechano11@gmail.com>
2026-03-17 08:07:31 -0700

    AeroTicket

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def home():
     return render_template("Home_page.html")
 
+@app.route("/login")
+def login():
+    return render_template("login_form.html")
+
 @app.route("/flight")
 def flight():
     flights = [
@@ -25,11 +29,41 @@ def flight():
 def check_in():
     return render_template("check_in.html")
   
-   
 @app.route("/about")
 def about():
     return render_template("About.html")
 
+@app.route("/ticket")
+def ticket():
 
+    ticket = {
+        "name": "Joe Hawkins",
+        "flight": "95678A",
+        "gate": "02",
+        "seat": "19A",
+        "origin": "MNL",
+        "destination": "CEBU",
+        "date": "15 JAN 2026",
+        "time": "06:30"
+    }
+
+    return render_template("ticket.html", ticket=ticket)
+
+@app.route("/booking")
+def booking():
+    return render_template("Booking.html")
+
+@app.route("/manage")
+def manage():
+    return render_template("Manage.html")
+
+@app.route("/travel")
+def travel():
+    return render_template("travel_info.html")
+ 
+@app.route("/explore")
+def explore():
+    return render_template("/Explore.html")
+    
 if __name__ == "__main__":
     app.run(debug=True)
